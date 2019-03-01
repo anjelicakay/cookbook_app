7979909b7aa2c2ef0fd5da3faf9a35d30644a395
anjelicakay <aktowner@gmail.com>
2019-03-01 09:30:00 -0600

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Recipe.create!([
+  {title: "Ratatouille", chef: "Remy", ingredients: "1 Large Zucchini, 1 Large Yellow Summer Squash, 2 Small Eggplants, 1 Red Bell Pepper, 1/2 Small Yellow Onion, 2 Garlic Cloves, 15 oz. can Crushed Tomatoes, Fresh Thyme, 1 tsp Salt", directions: "Mix them", image_url: "https://media.chefdehome.com/740/0/0/ratatouille/ratatouille-casserole.jpg", prep_time: 43},
+  {title: "Jelly Beans", chef: "Toot Sweet", ingredients: "sugar, sugar and more sugar", directions: "mix compact into candy pill form", image_url: "http://voguesugar.com/wp-content/uploads/2014/03/jellybeans.jpg", prep_time: 16},
+  {title: "Cookies", chef: "Cookie Monster", ingredients: "cookie mix", directions: "cookie, cookie, darn should have bought eggs", image_url: "http://1.bp.blogspot.com/-w-i7-dRn_V0/TeZP9aAxPBI/AAAAAAAAAR4/8E_-JCJlEmQ/s1600/Apri2011+055pwm.jpg", prep_time: 380},
+  {title: "Raw Egg", chef: "Bad Josh", ingredients: "live chicken, pair of gloves", directions: "place gloves on hands, grip chicken body, squeeze gently", image_url: "https://memepedia.ru/wp-content/uploads/2019/01/47692668_1958135090974774_6762833792332802352_n.jpg", prep_time: 5},
+  {title: "Lagsana", chef: "Garfield", ingredients: "tire, old shoe, peppermint toothpaste, noodles", directions: "pile ingredients into pan, heat to 350 degrees, wait for fire alarm", image_url: "https://media1.fdncms.com/orlando/imager/u/original/2511867/lasagna.jpg", prep_time: 400},
+  {title: "Hand Sandwhich", chef: "Josh", ingredients: "hand, bread", directions: "place left hand palm up, lay other ingredients on top, finish with right hand, place hands together, good night, wisconsin!", image_url: "https://i.pinimg.com/originals/3a/1a/4e/3a1a4e0fb7c54c1894ecc08d64851628.jpg", prep_time: 400}
+])
+
